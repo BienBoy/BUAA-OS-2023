4ae11073c105722b66db3f2b5643404355847d41
徐天行 <20241029@buaa.edu.cn>
2023-03-06 20:09:53 +0800

    初次作答

--- analyze.sh
+++ analyze.sh
@@ -3,20 +3,23 @@
 if [ $# -eq 1 ];
 then
     # Your code here. (1/4)
+    grep -E "WARN|ERROR" $1 > bug.txt
 
 else
     case $2 in
     "--latest")
         # Your code here. (2/4)
+	tail -n 5 $1
 
     ;;
     "--find")
         # Your code here. (3/4)
+	grep $3 $1 > $3.txt
 
     ;;
     "--diff")
         # Your code here. (4/4)
-
+	diff -s -q $1 $3 | sed s/.*相同.*/same/ | sed s/.*不同.*/different/
     ;;
     esac
 fi
