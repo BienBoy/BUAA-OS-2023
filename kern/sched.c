fdf16edb996a850f22a35523f6c3684d7e916793
徐天行 <20241029@buaa.edu.cn>
2023-03-27 23:35:05 +0800

    初次作答

--- kern/sched.c
+++ kern/sched.c
@@ -35,5 +35,17 @@ void schedule(int yield) {
 	 *   'TAILQ_FIRST', 'TAILQ_REMOVE', 'TAILQ_INSERT_TAIL'
 	 */
 	/* Exercise 3.12: Your code here. */
+	count--;
+	if (!e || !count || e->env_status != ENV_RUNNABLE || yield) {
+		if (e)	
+			TAILQ_REMOVE(&env_sched_list, e, env_sched_link);
+		if (e && e->env_status == ENV_RUNNABLE)
+			TAILQ_INSERT_TAIL(&env_sched_list, e, env_sched_link);
+		if (TAILQ_EMPTY(&env_sched_list))
+			panic("schedule: no runnable envs");
+		e = TAILQ_FIRST(&env_sched_list);
+		count = e->env_pri;
+	}
 
+	env_run(e);
 }
